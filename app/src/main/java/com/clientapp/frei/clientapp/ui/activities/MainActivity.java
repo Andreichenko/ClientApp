f8be77f1dec73212fc0e50bdf440eee1c66aba7b
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-16 22:02:20 +0300

    added descriptions

--- app/src/main/java/com/clientapp/frei/clientapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/clientapp/frei/clientapp/ui/activities/MainActivity.java
@@ -55,11 +55,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * method is called when you start activity before the time that the UI will become available to the user.
+     * method is called when you start activity before the time that the UI will become available to
+     * the user.
      * usually in this method registers event subscriptions that stop was made in onStop()
      *
-     * Methode wird aufgerufen, wenn Sie beginnen, Aktivität vor der Zeit, dass die Benutzeroberfläche dem Benutzer zur Verfügung gestellt.
-     * In der Regel, die in dieser Methode registriert-Ereignis-Abonnements, dass halt gemacht wurde, in onStop()
+     * Methode wird aufgerufen, wenn Sie beginnen, Aktivität vor der Zeit, dass die Benutzeroberfläche
+     * dem Benutzer zur Verfügung gestellt.
+     * In der Regel, die in dieser Methode registriert-Ereignis-Abonnements, dass halt gemacht wurde,
+     * in onStop()
      */
 
     @Override
@@ -71,11 +74,13 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * method is invoked when the activity becomes available for user interaction.
-     * in this method, usually run animations/audio/video/run BroadcastReceiver needed to implement UI logic/start execution of threads, etc.
+     * in this method, usually run animations/audio/video/run BroadcastReceiver needed to implement UI
+     * logic/start execution of threads, etc.
      * the method should be as lightweight, for maximum responsiveness of the UI
      *
      * Methode wird aufgerufen, wenn die Aktivität verfügbar für Benutzer-Interaktion.
-     * In dieser Methode, in der Regel laufen Animationen/audio/video/run BroadcastReceiver benötigt um UI-Logik/starten Sie die Ausführung von threads, etc.
+     * In dieser Methode, in der Regel laufen Animationen/audio/video/run BroadcastReceiver benötigt
+     * um UI-Logik/starten Sie die Ausführung von threads, etc.
      * die Methode sollte so leicht, für eine maximale Reaktionsfähigkeit der Benutzeroberfläche
      *
      */
@@ -87,6 +92,18 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onResume");
     }
 
+    /**
+     *
+     * method is called when the current activity loses focus but remains visible (the ascent of
+     * the dialog box/partial overlap other activity, etc.)
+     * this method is implemented by maintaining a lightweight UI data/animations/audio/video etc.)
+     *
+     * Methode wird aufgerufen, wenn die aktuelle Aktivität verliert den Fokus, bleibt aber sichtbar
+     * (der Aufstieg der dialog-box/teilweise überlappung mit anderen Aktivitäten, usw.)
+     * diese Methode wird implementiert, indem die Aufrechterhaltung einer leichten UI-Daten/Animation/audio/video usw.)
+     *
+     */
+
 
     @Override
     protected void onPause() {
@@ -95,6 +112,18 @@ public class MainActivity extends AppCompatActivity {
          Log.d(TAG, "onPause");
     }
 
+    /**
+     *
+     * method is invoked when the activity becomes invisible to the user. in this method occurs unsubscribing
+     * from events, stop, complex animations, complex operations for saving data/interruption of threads running, etc.
+     *
+     *Methode wird aufgerufen, wenn die activity wird unsichtbar für den Benutzer.
+     * in dieser Methode erfolgt Abmeldung von Veranstaltungen, stoppen, komplexe Animationen,
+     * komplexe Vorgänge für das speichern von Daten/Unterbrechung von threads, und so weiter.
+     *
+     *
+     */
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -102,12 +131,41 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG,"onStop");
     }
 
+    /**
+     *
+     *
+     * method is called when the end of the work activity (when it occurs systemically, or after
+     * call finish()
+     *
+     * Methode wird aufgerufen, wenn das Ende der Tätigkeit (wenn es Auftritt, systemisch,
+     * oder nach Aufruf finish()
+     *
+     */
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
         Log.d(TAG, "onDestroy");
     }
+
+    /**
+     *
+     * method is called when the restart activiti/resume after call onStop()method
+     * this method implements the specific business logic that should be realized when you restart
+     * the activity - for example, the query to the server, which must be called when returning
+     * from another activity (data updates, subscription to a particular event initialized on another
+     * screen/specific business logic tied it to restart activity
+     *
+     *
+     * Methode wird aufgerufen, wenn der Neustart activiti/resume nach Aufruf onStop () - Methode
+     * diese Methode implementiert die spezifische business-Logik, die realisiert werden sollte,
+     * wenn sie neu startet, die Aktivität - zum Beispiel, die Abfrage an den server, die angerufen werden muss,
+     * bei der Rückkehr von einer anderen Aktivität (Daten-updates, das Abonnement eines bestimmten
+     * Ereignisses initialisiert, auf einem anderen Bildschirm/spezifische business-Logik gebunden,
+     * die es neu zu starten, activity
+     *
+     */
 
     @Override
     protected void onRestart() {
