9681d8e134c237dc5ba0750bbfa759ab07b75a82
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-16 15:21:42 +0300

    Initial commit

---
+++ app/src/main/java/com/clientapp/frei/clientapp/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clientapp.frei.clientapp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clientapp.frei.clientapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
