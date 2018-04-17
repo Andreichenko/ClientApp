e787814807cab8e567117c98d3645a7977bcf097
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-18 01:18:20 +0300

    fix

--- app/src/main/java/com/clientapp/frei/clientapp/ui/activities/BaseActivity.java
+++ app/src/main/java/com/clientapp/frei/clientapp/ui/activities/BaseActivity.java
@@ -13,7 +13,7 @@ public class BaseActivity extends AppCompatActivity{
 
     public void showProgress(){
         if (mProgressDialog == null){
-      // todo       mProgressDialog = new ProgressDialog();
+      // todo       mProgressDialog = new ProgressDialog(); api 16???
         }
     }
 }
