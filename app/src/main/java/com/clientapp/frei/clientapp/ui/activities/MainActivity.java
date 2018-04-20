8615ef6c73110596f0793de5e572064bced8998e
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-20 13:11:28 +0300

    Added  private CoordinatorLayout mCoordinatorLayout;           private Toolbar mToolbar;           private DrawerLayout mNavigationDrawer;           private FloatingActionButton mFab;           private EditText mUserPhone, mUserMail, mUserBio;
              private List<EditText> mUserInfo;

--- app/src/main/java/com/clientapp/frei/clientapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/clientapp/frei/clientapp/ui/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package com.clientapp.frei.clientapp.ui.activities;
 
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.clientapp.frei.clientapp.R;
 import com.clientapp.frei.clientapp.utils.ConstantManager;
+
+import java.util.List;
 
 /**
  *
@@ -24,7 +30,16 @@ public class MainActivity extends BaseActivity {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "MainActivity";
 
+    private int mCurrentEditMode = 0;
+
+
     private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserBio;
+
+    private List<EditText> mUserInfo;
 
     /**
      *  This method is called when creating activity (after a configuration change/return to the current
