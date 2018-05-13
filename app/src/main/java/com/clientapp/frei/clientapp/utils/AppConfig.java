cf4e6231b24a138f39d58f3c413f81d026306afd
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-13 23:14:23 +0300

    Added AppConfig

---
+++ app/src/main/java/com/clientapp/frei/clientapp/utils/AppConfig.java
@@ -0,0 +1,10 @@
+package com.clientapp.frei.clientapp.utils;
+
+public class AppConfig {
+
+    String BASE_URL = "http://serverui1.-apps.de/api/";
+
+    int MAX_CONNECT_TIMEOUT = 5000;
+    int MAX_READ_TIMEOUT = 5000;
+
+}
