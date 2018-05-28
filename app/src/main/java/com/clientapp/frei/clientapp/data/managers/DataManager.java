99630ac889926172cf2accea62a296c8de981e79
aleksandrandreichenko <rsn_alex@mail.ru>
2018-05-28 22:25:24 +0300

    add Manager

--- app/src/main/java/com/clientapp/frei/clientapp/data/managers/DataManager.java
+++ app/src/main/java/com/clientapp/frei/clientapp/data/managers/DataManager.java
@@ -2,9 +2,12 @@ package com.clientapp.frei.clientapp.data.managers;
 
 
 import android.content.Context;
+import android.util.Log;
 
 import com.clientapp.frei.clientapp.ui.activities.MainActivity;
 import com.clientapp.frei.clientapp.utils.ConstantManager;
+
+import java.util.ArrayList;
 
 public class DataManager {
 
@@ -36,6 +39,76 @@ public class DataManager {
         }
         return INSTANCE;
     }
+    public PreferencesManager getPreferencesManager() {
+        return mPreferencesManager;
+    }
 
+    public Context getContext() {
+        return mContext;
+    }
+
+    public Picasso getPicasso() {
+        return mPicasso;
+    }
+
+    public RestService getRestService() {
+        return mRestService;
+    }
+
+    //region --- Network ---
+    public Call<UserLoginRes> loginUser(UserLoginReq userLoginReq) {
+        return mRestService.loginUser(userLoginReq);
+    }
+
+    public Call<UserDataRes> loginToken(String userId) {
+        return mRestService.loginToken(userId);
+    }
+
+    public Call<UploadPhotoRes> uploadPhoto(String userId, MultipartBody.Part file) {
+        return mRestService.uploadPhoto(userId, file);
+    }
+
+    public Call<UploadPhotoRes> uploadAvatar(String userId, MultipartBody.Part file) {
+        return mRestService.uploadAvatar(userId, file);
+    }
+
+    public Call<UserListRes> getUserListFromNetwork() {
+        return mRestService.getUserList();
+    }
+
+    public Call<UserContactsRes> changeUserContacts(String userId, UserContactsReq userContactsReq) {
+        return mRestService.changeUserContacts(userId, userContactsReq);
+    }
+
+    public Call<UserLikeRes> likeUser(String userId) {
+        return mRestService.likeUser(userId);
+    }
+
+    public Call<UserUnlikeRes> unlikeUser(String userId) {
+        return mRestService.unlikeUser(userId);
+    }
+    //endregion
+
+    //region --- Database ---
+    public DaoSession getDaoSession() {
+        return mDaoSession;
+    }
+
+    public List<User> getUserListByName(String query) {
+        List<User> userList = new ArrayList<>();
+
+        try {
+            userList = mDaoSession.queryBuilder(User.class)
+                    .where(UserDao.Properties.Rating.gt(0), UserDao.Properties.SearchName.like("%" + query.toUpperCase() + "%"))
+                    .orderDesc(UserDao.Properties.Rating)
+                    .build()
+                    .list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "getUserListByName: " + e.getMessage());
+        }
+
+        return userList;
+    }
 
 }
--- app/src/main/java/com/clientapp/frei/clientapp/data/managers/PreferencesManager.java
+++ app/src/main/java/com/clientapp/frei/clientapp/data/managers/PreferencesManager.java
@@ -1,4 +1,91 @@
 package com.clientapp.frei.clientapp.data.managers;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import com.clientapp.frei.clientapp.utils.ConstantManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PreferencesManager {
+
+
+    private static final String[] USER_DATA = {
+            ConstantManager.FIRST_NAME,
+            ConstantManager.SECOND_NAME};
+    private static final String[] USER_INFO = {
+            ConstantManager.USER_PHONE_KEY,
+            ConstantManager.USER_MAIL_KEY,
+            ConstantManager.USER_VK_KEY,
+            ConstantManager.USER_GIT_KEY,
+            ConstantManager.USER_BIO_KEY};
+    private static final String[] USER_PROFILE = {
+            ConstantManager.USER_RATING_VALUE,
+            ConstantManager.USER_CODE_LINES_VALUE,
+            ConstantManager.USER_PROJECT_VALUE};
+    private static final int[] USER_INFO_DEFAULT_VALUES = {
+            R.string.user_profile_phone_et_default_value,
+            R.string.user_profile_email_et_default_value,
+            R.string.user_profile_vk_et_default_value,
+            R.string.user_profile_git_et_default_value,
+            R.string.user_profile_bio_et_default_value};
+    private SharedPreferences mSharedPreferences;
+
+    public PreferencesManager() {
+        this.mSharedPreferences = Application.getSharedPreferences();
+    }
+
+
+    public void saveUserFullName(List<String> userData) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i < USER_DATA.length; i++) {
+            editor.putString(USER_DATA[i], userData.get(i));
+        }
+        editor.apply();
+    }
+
+    public String getFullName() {
+        return mSharedPreferences.getString(ConstantManager.SECOND_NAME, "null") + " " +
+                mSharedPreferences.getString(ConstantManager.FIRST_NAME, "null");
+    }
+
+    public void saveUserInfoData(List<String> userInfo) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i < USER_INFO.length; i++) {
+            editor.putString(USER_INFO[i], userInfo.get(i));
+        }
+        editor.apply();
+    }
+
+    public List<String> loadUserInfoData() {
+        List<String> userInfo = new ArrayList<>();
+        for (int i = 0; i < USER_INFO.length; i++) {
+            userInfo.add(mSharedPreferences.getString(USER_INFO[i],
+                    DevIntensiveApplication.getContext().getString(USER_INFO_DEFAULT_VALUES[i])));
+        }
+        return userInfo;
+    }
+
+
+    public void saveUserProfileData(int[] userProfile) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i < USER_PROFILE.length; i++) {
+            editor.putString(USER_PROFILE[i], String.valueOf(userProfile[i]));
+        }
+        editor.apply();
+    }
+
+    public List<String> loadUserProfileData() {
+        List<String> userProfile = new ArrayList<>();
+        for (int i = 0; i < USER_PROFILE.length; i++) {
+            userProfile.add(mSharedPreferences.getString(USER_PROFILE[i], "0"));
+        }
+        return userProfile;
+    }
+
+
 }
