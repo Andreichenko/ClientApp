71093f3e456a23a1f227ca4e7ef99ef2175eba50
aleksandrandreichenko <rsn_alex@mail.ru>
2018-06-10 00:30:13 +0300

    class work

--- app/src/main/java/com/clientapp/frei/clientapp/ui/activities/AuthActivity.java
+++ app/src/main/java/com/clientapp/frei/clientapp/ui/activities/AuthActivity.java
@@ -1,4 +1,33 @@
 package com.clientapp.frei.clientapp.ui.activities;
 
-public class AuthActivity {
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.clientapp.frei.clientapp.data.managers.DataManager;
+import com.clientapp.frei.clientapp.utils.ConstantManager;
+
+public class AuthActivity extends BaseActivity implements View.OnClickListener{
+
+    public static final String TAG = ConstantManager.TAG_PREFIX + "AuthActivity";
+
+    @BindView(R.id.main_coordinator_container)
+    CoordinatorLayout mCoordinatorLayout;
+    @BindView(R.id.authorization_box)
+    CardView mAuthorizationBox;
+    @BindView(R.id.login_btn)
+    Button mSignIn;
+    @BindView(R.id.remember_txt)
+    TextView mRememberPassword;
+    @BindView(R.id.login_email_et)
+    EditText mLogin;
+    @BindView(R.id.login_password_et)
+    EditText mPassword;
+
+    private ChronosConnector mConnector;
+    private Bus mBus;
+
+    private DataManager mDataManager;
 }
